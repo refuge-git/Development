3f57857d11799f1dcc0c0dfaeb6881dbd196b9bc
raulreis123 <raulgomesreis@gmail.com>
2025-03-09 19:25:38 -0300

    feature: Configuração de servidor e controller

--- servidor-web/src/main/java/school/sptech/refuge/RefugeApplication.java
+++ servidor-web/src/main/java/school/sptech/refuge/RefugeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RefugeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(RefugeApplication.class, args);
 	}
 
 }
---
+++ servidor-web/src/main/java/school/sptech/refuge/UsuarioController.java
@@ -0,0 +1,4 @@
+package school.sptech.refuge;
+
+public class UsuarioController {
+}
