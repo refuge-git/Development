64d4cb33d02774d9e80ef5ef9b1dacaed3187001
raulreis123 <raulgomesreis@gmail.com>
2025-03-10 13:38:02 -0300

    feature: Duas novas querys no JPA: findBynome...(), findByRaca...()

--- servidor-web/src/main/java/school/sptech/refuge/UsuarioRepository.java
+++ servidor-web/src/main/java/school/sptech/refuge/UsuarioRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+// !!! MATERIAL DE REFERÊNCIA: SpringBoot com JPA (disponível no moodle) !!!
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    // Construção: Retorno, nome e parâmetro
+    // Construção da query JPA: Retorno, nome e parâmetro
     // OBS: Vale ressaltar que o JPA interpreta a query pelo nome dos métodos!!!
     Optional<Usuario> findByCpf (String cpf); // Encontrar por CPF
     List<Usuario> findByGenero (Genero genero); // Encontrar todos por genero
+    List<Usuario> findByNomeContainingIgnoreCase (String nome); // Encontrar usuários que contenham esse nome, ignorando maiúsculas e minúsculas
+    List<Usuario> findByRacaContainingIgnoreCase (String raca); // Encontrar usuários que contenham essa raça, ignorando maiúsculas e minúsculas
 }
