9c094545eebc7066d4f12e94f1eb227a2bd56670
raulreis123 <raulgomesreis@gmail.com>
2025-03-20 02:34:52 -0300

    feature: entidade usuario com getters e setters

--- servidor-web/src/main/java/school/sptech/refuge/funcionario/Funcionario.java
+++ servidor-web/src/main/java/school/sptech/refuge/funcionario/Funcionario.java
@@ -1,5 +1,57 @@
 package school.sptech.refuge.funcionario;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Funcionario {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
