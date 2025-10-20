be19886b736cd4f7d4917e016516a667a08f5779
raulreis123 <raulgomesreis@gmail.com>
2025-10-19 21:55:49 -0300

    feature: Permissões no bucket S3

--- servidor-web-clean-arch/src/main/java/school/sptech/refuge/infrastructure/config/bucketS3/ConfiguracaoS3.java
+++ servidor-web-clean-arch/src/main/java/school/sptech/refuge/infrastructure/config/bucketS3/ConfiguracaoS3.java
@@ -3,8 +3,10 @@ package school.sptech.refuge.infrastructure.config.bucketS3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class ConfiguracaoS3 {
@@ -15,6 +17,14 @@ public class ConfiguracaoS3 {
     public S3Client clienteS3() {
         return S3Client.builder()
                 .region(Region.of(awsRegion))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
--- servidor-web-clean-arch/src/main/java/school/sptech/refuge/infrastructure/config/bucketS3/S3UploadService.java
+++ servidor-web-clean-arch/src/main/java/school/sptech/refuge/infrastructure/config/bucketS3/S3UploadService.java
@@ -7,8 +7,8 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
-// import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-// import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 import java.time.Duration;
 
@@ -16,17 +16,12 @@ import java.time.Duration;
 public class S3UploadService {
 
     private final S3Client s3Client;
-    // private final S3Presigner s3Presigner;
+    private final S3Presigner s3Presigner;
 
     // Comentado para evitar erro de bean ausente
-    // public S3UploadService(S3Client s3Client, S3Presigner s3Presigner) {
-    //     this.s3Client = s3Client;
-    //     this.s3Presigner = s3Presigner;
-    // }
-
-    // Construtor alternativo sem S3Presigner
-    public S3UploadService(S3Client s3Client) {
+    public S3UploadService(S3Client s3Client, S3Presigner s3Presigner) {
         this.s3Client = s3Client;
+        this.s3Presigner = s3Presigner;
     }
 
     @Value("${aws.s3.bucket-refuge}")
@@ -52,15 +47,12 @@ public class S3UploadService {
 
     public String getFile(String key) {
         try {
-            // Comentado para evitar erro de bean ausente
-            // GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
-            //         .getObjectRequest(r -> r.bucket(bucketName).key(key))
-            //         .signatureDuration(Duration.ofMinutes(10))
-            //         .build();
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .getObjectRequest(r -> r.bucket(bucketName).key(key))
+                    .signatureDuration(Duration.ofMinutes(10))
+                    .build();
 
-            // return s3Presigner.presignGetObject(presignRequest).url().toString();
-
-            return "Pré-assinatura desativada temporariamente.";
+            return s3Presigner.presignGetObject(presignRequest).url().toString();
         } catch (S3Exception e) {
             throw new ResponseStatusException(500,
                     "Erro ao gerar URL pré-assinada do S3: " + e.awsErrorDetails().errorMessage(),
