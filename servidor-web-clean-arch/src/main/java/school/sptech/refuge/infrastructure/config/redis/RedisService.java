357fbcd8d68a44343a54617d1f3522ca0d327922
raulreis123 <raulgomesreis@gmail.com>
2025-11-26 19:08:02 -0300

    fix: Correção para funcionamento do sistema cache redis

--- servidor-web-clean-arch/src/main/java/school/sptech/refuge/infrastructure/config/redis/RedisService.java
+++ servidor-web-clean-arch/src/main/java/school/sptech/refuge/infrastructure/config/redis/RedisService.java
@@ -1,14 +1,19 @@
 package school.sptech.refuge.infrastructure.config.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import school.sptech.refuge.core.application.dto.beneficiario.BeneficarioListDto;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RedisService {
+    @Autowired
+    private ObjectMapper objectMapper;
     private final RedisTemplate<String, Object> redisTemplate;
 
     public RedisService(RedisTemplate<String, Object> redisTemplate) {
@@ -17,9 +22,18 @@ public class RedisService {
 
     private static final String CACHE_KEY = "lista-beneficiarios";
 
-    // 1. Buscar no cache
     public List<BeneficarioListDto> getBeneficiarios() {
-        return (List<BeneficarioListDto>) redisTemplate.opsForValue().get(CACHE_KEY);
+        Object raw = redisTemplate.opsForValue().get(CACHE_KEY);
+
+        if (raw == null) return null;
+
+        List<?> list = (List<?>) raw;
+
+        System.out.println("Retornando do cache");
+
+        return list.stream()
+                .map(obj -> objectMapper.convertValue(obj, BeneficarioListDto.class))
+                .collect(Collectors.toList());
     }
 
     // 2. Salvar no cache
